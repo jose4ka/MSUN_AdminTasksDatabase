4fa14ca345b365fe2606bdd3ad7389cc2fb004d0
dmitriy <rabowvdk125@gmail.com>
2020-08-26 15:09:40 +1000

    visual

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -54,56 +54,53 @@ Vue.component('task-editor', {
     },
 
     template:
-                '<div>'+
-                '<input type="date" placeholder="Дата создания" v-model="dateCreation"/>'+
-                '<input type="time" placeholder="Время создания" v-model="timeCreation"/>'+
-                '<input type="text" placeholder="Ф.И.О. заявителя" v-model="owner"/>'+
-                '<input type="text" placeholder="Подразделение" v-model="subdivision" value="СКТУ и ВЭО"/>'+
 
-                '<input list="blocks" placeholder="Корпус" v-model="block"/>'+
-                                    '<datalist id="blocks">'+
-                                      '<option>Корпус №1 (Верхнепортовая 50а)</option>'+
-                                      '<option>Корпус №2 (Верхнепортовая 50б</option>'+
-                                      '<option>Корпус №3 (Станюковича 60а)</option>'+
-                                      '<option>Корпус №5 (Станюковича 58)</option>'+
-                                      '<option>Корпус №6 (Станюковича 66)</option>'+
-                                      '<option>Корпус №7 (Станюковича 64)</option>'+
-                                      '<option>Корпус №8 (Авраменко 16)</option>'+
-                                      '<option>Корпус №9 (Станюковича 62)</option>'+
-                                      '<option>Корпус №10 (Станюковича 62)</option>'+
-                                      '<option>Корпус №12 (Авраменко 9)</option>'+
-                                      '<option>Корпус №14 (Тренажерный комплекс)</option>'+
-                                      '<option>Водная станция (Лейтентанта Шмидта 29)</option>'+
-                                    '</datalist>'+
 
-                '<input type="text" placeholder="Кабинет" v-model="cabinet"/>'+
+                    '<table class="flat-table">'+
+                    '<tr> <td> <p>Описание</p> </td> <td> <p class="uptext">Значение</p> </td> </tr>'+
+                    '<tr> <td> <p>Дата создания</p> </td> <td> <input type="date" placeholder="Дата создания" v-model="dateCreation"/> </td> </tr>'+
+                    '<tr> <td> <p>Время создания</p> </td> <td> <input type="time" placeholder="Время создания" v-model="timeCreation"/> </td> </tr>'+
+                    '<tr> <td> <p>Заявитель</p>  </td> <td> <input type="text" placeholder="Ф.И.О. заявителя" v-model="owner"/>  </td> </tr>'+
+                    '<tr> <td> <p>Подразделение</p> </td> <td> <input type="text" placeholder="Подразделение" v-model="subdivision" value="СКТУ и ВЭО"/> </td> </tr>'+
+                    '<tr> <td> <p>Корпус</p> </td> <td> <input list="blocks" placeholder="Корпус" v-model="block"/>'+
+                                                                                                           '<datalist id="blocks">'+
+                                                                                                             '<option>Корпус №1 (Верхнепортовая 50а)</option>'+
+                                                                                                             '<option>Корпус №2 (Верхнепортовая 50б</option>'+
+                                                                                                             '<option>Корпус №3 (Станюковича 60а)</option>'+
+                                                                                                             '<option>Корпус №5 (Станюковича 58)</option>'+
+                                                                                                             '<option>Корпус №6 (Станюковича 66)</option>'+
+                                                                                                             '<option>Корпус №7 (Станюковича 64)</option>'+
+                                                                                                             '<option>Корпус №8 (Авраменко 16)</option>'+
+                                                                                                             '<option>Корпус №9 (Станюковича 62)</option>'+
+                                                                                                             '<option>Корпус №10 (Станюковича 62)</option>'+
+                                                                                                             '<option>Корпус №12 (Авраменко 9)</option>'+
+                                                                                                             '<option>Корпус №14 (Тренажерный комплекс)</option>'+
+                                                                                                             '<option>Водная станция (Лейтентанта Шмидта 29)</option>'+
+                                                                                                           '</datalist> </td> </tr>'+
+                    '<tr> <td> <p>Кабинет</p> </td> <td> <input type="text" placeholder="Кабинет" v-model="cabinet"/>  </td> </tr>'+
+                    '<tr> <td> <p>Категория</p> </td> <td> <input list="types" placeholder="Категория" v-model="type"/>'+
+                                                                                                             '<datalist id="types">'+
+                                                                                                              '<option>Телефония</option>'+
+                                                                                                              '<option>Сеть</option>'+
+                                                                                                              '<option>Классы</option>'+
+                                                                                                              '<option>Программы</option>'+
+                                                                                                             '</datalist> </td> </tr>'+
+                    '<tr> <td> <p>Телефон для связи</p> </td> <td> <input type="text" placeholder="Телефон для связи" v-model="phoneNumber"/> </td> </tr>'+
+                    '<tr> <td> <p>Описание проблемы</p> </td> <td> <input type="text" placeholder="Описание проблемы" v-model="text"/> </td> </tr>'+
+                    '<tr> <td> <p>Статус выполнения</p> </td> <td> <input list="perfStatuses" placeholder="Статус выполнения" v-model="performanceStatus"/>'+
+                                                                                                                                     '<datalist id="perfStatuses">'+
+                                                                                                                                      '<option>Принята</option>'+
+                                                                                                                                      '<option>Выполнена</option>'+
+                                                                                                                                      '<option>Ожидание</option>'+
+                                                                                                                                      '<option>Невыполнима</option>'+
+                                                                                                                                     '</datalist> </td> </tr>'+
+                    '<tr> <td> <p>Дата выполнения</p> </td> <td> <input type="date" placeholder="Дата выполнения" v-model="datePerformance"/></td> </tr>'+
+                    '<tr> <td> <p>Исполнители</p> </td> <td> <input type="text" placeholder="Исполнители" v-model="executors"/> </td> </tr>'+
+                    '<tr> <td> <p>Комментарий</p> </td> <td> <textarea name="comm" cols="40" rows="5" placeholder="Комментарий" v-model="comment"></textarea> </td> </tr>'+
+                    '<tr> <td> <input type="button" value="Reset" @click="reset"/> </td> <td> <input type="button" value="Save" @click="save"/>  </td> </tr>'+
 
-                '<input list="types" placeholder="Категория" v-model="type"/>'+
-                                   '<datalist id="types">'+
-                                    '<option>Телефония</option>'+
-                                    '<option>Сеть</option>'+
-                                    '<option>Классы</option>'+
-                                    '<option>Программы</option>'+
-                                   '</datalist>'+
+                    '</table>',
 
-                '<input type="text" placeholder="Телефон для связи" v-model="phoneNumber"/>'+
-                '<input type="text" placeholder="Описания проблемы" v-model="text"/>'+
-
-                '<input list="perfStatuses" placeholder="Статус выполнения" v-model="performanceStatus"/>'+
-                                                   '<datalist id="perfStatuses">'+
-                                                    '<option>Принята</option>'+
-                                                    '<option>Выполнена</option>'+
-                                                    '<option>Ожидание</option>'+
-                                                    '<option>Невыполнима</option>'+
-                                                   '</datalist>'+
-
-                '<input type="date" placeholder="Дата выполнения" v-model="datePerformance"/>'+
-                '<input type="text" placeholder="Исполнители" v-model="executors"/>'+
-                '<input type="text" placeholder="Комментарий" v-model="comment"/>'+
-
-                '<input type="button" value="Save" @click="save"/>'+
-                '<input type="button" value="Reset" @click="reset"/>'+
-                '</div>',
 
     methods: {
             save: function(){
@@ -211,20 +208,34 @@ Vue.component('task-row', {
 
     template:
     '<div>'+
-    '<i>(  {{ task.id}}  )</i>'+
-    '<span style="position: absolute; left: 0">'+
 
-            '<input type="button" value="X" @click="del" />'+
-            '<input type="button" value="Edit" @click="edit" />'+
-
-            '({{task.id}})'+
-            '  {{task.text}}  '+
-            '  {{task.dateCreation}}  '+
-            '  {{task.type}}  '+
-            '  {{task.owner}}  '+
-            '  {{task.block}}  '+
-            '  +{{task.phoneNumber}}  '+
-    '</span>'+
+    '<table class="flat-table">'+
+      '<tr>'+
+        '<td><input type="button" value="X" @click="del" /></td>'+
+        '<td><input type="button" value="Edit" @click="edit" /></td>'+
+        '<td  width="100px">(  {{ task.id}}  )</td>'+
+        '<td  width="100px">{{task.text}} </td>'+
+        '<td  width="100px">{{task.dateCreation}}</td>'+
+        '<td  width="100px">{{task.type}} </td>'+
+        '<td  width="100px">{{task.owner}}</td>'+
+        '<td  width="100px">{{task.block}} </td>'+
+        '<td  width="100px">+{{task.phoneNumber}}</td>'+
+      '</tr>'+
+    '</table>'+
+//    '<i>(  {{ task.id}}  )</i>'+
+//    '<span style="position: absolute; left: 0">'+
+//
+//            '<input type="button" value="X" @click="del" />'+
+//            '<input type="button" value="Edit" @click="edit" />'+
+//
+//            '({{task.id}})'+
+//            '  {{task.text}}  '+
+//            '  {{task.dateCreation}}  '+
+//            '  {{task.type}}  '+
+//            '  {{task.owner}}  '+
+//            '  {{task.block}}  '+
+//            '  +{{task.phoneNumber}}  '+
+//    '</span>'+
     '</div>',
 
     methods:{
@@ -260,9 +271,13 @@ Vue.component('tasks-list', {
 
     },
     template:
-    '<div style="position: relative; width: 1000px;">'+
+    '<div style="position: relative;">'+
+
         '<task-editor :tasks="tasks" :taskAttr="task"/>'+
+
         '<task-row v-for="task in tasks" :key="task.id" :task="task" :editMethod="editMethod" :tasks="tasks"/>'+
+
+
     '</div>',
 
     //После создания - заполняем данными с сервера
