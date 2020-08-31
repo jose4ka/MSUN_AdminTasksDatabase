bf595598a3f2c045f1cf1e0b361a6c4ebd421ba4
loakdv <rabowvdk125@gmail.com>
2020-09-01 08:16:47 +1000

    bebra

--- src/main/java/com/msun/telegramBot/bot/command/HelpCommand.java
+++ src/main/java/com/msun/telegramBot/bot/command/HelpCommand.java
@@ -4,10 +4,12 @@ import com.msun.telegramBot.bot.Bot;
 import com.msun.telegramBot.bot.Command;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+import java.util.Random;
+
 public class HelpCommand extends Command {
 
     @Override
     public void execute(Bot bot, Message message) {
-        bot.sendMsg(message.getChatId().toString(), "Biba а не help (пока-что)");
+        bot.sendMsg(message.getChatId().toString(), new Random().nextBoolean() ? "Бебра":"Biba а не help (пока-что)");
     }
 }
